9adde55ecd8bbc5fe3a3864639e29013dea942ab
Akash16092002 <143410962+Akash16092002@users.noreply.github.com>
2023-09-28 20:50:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,61 @@
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
+document.addEventListener("DOMContentLoaded", function(event) {
+    let body = document.querySelector('body');
+    let result = document.querySelector('#result');
+    
+    let dark_mode_btn = document.querySelector('.dark_mode_btn');
+    let clear = document.querySelector('#clear');
+    let history = document.querySelector('#history');
+    let equalTo = document.querySelector('#equalTo');
+    let delete_single_num = document.querySelector('#delete_single_num');
+    
+    let Normal_btn = document.querySelectorAll('#Normal_btn');
+    
+    
+    let initial_value = "";
+    
+    Normal_btn.forEach((Normal_btn, index)=>{
+    Normal_btn.addEventListener('click', function(){
+    let text = this.innerHTML;
+    initial_value += text;
+    result.innerHTML = initial_value;
+    });
+    });
+    
+
+    equalTo.addEventListener('click', function(){
+    if (result.innerHTML != "") {
+    history.innerHTML = result.innerHTML;
+    result.innerHTML = eval(result.innerHTML);
+    initial_value = eval(result.innerHTML);
+    }else{
+    alert('please enter any Number');
     }
-    else{
-        document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                    ${document.querySelector('#newtask input').value}
-                </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
-
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-
-        var tasks = document.querySelectorAll(".task");
-        for(var i=0; i<tasks.length; i++){
-            tasks[i].onclick = function(){
-                this.classList.toggle('completed');
-            }
-        }
-
-        document.querySelector("#newtask input").value = "";
+    });
+    
+    
+    let dark_mode_status = false;
+    dark_mode_btn.addEventListener('click', function(){
+    body.classList.toggle('dark_mode_active');
+    if (dark_mode_status == false) {
+    this.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>';
+    dark_mode_status = true;
+    }else{
+    this.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
+    dark_mode_status = false;
     }
-}
+    });
+    
+    
+
+    clear.addEventListener('click', function(){
+    result.innerHTML = "";
+    initial_value = "";
+    });
+    
+
+    delete_single_num.addEventListener('click', function(){
+    result.innerHTML = result.innerHTML.substring(0,result.innerHTML.length-1);
+    initial_value = result.innerHTML;
+    });
+    
+    });
